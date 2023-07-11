e6c3dac87901d4157fd40b30a4ac1097c2ba0b64
unknown <rizalyuniar123@gmail.com>
2023-07-12 00:20:33 +0700

    resolve deploy again

--- components/Users/ListComment.jsx
+++ components/Users/ListComment.jsx
@@ -19,9 +19,9 @@ const ListComment = () => {
         <h3>Kumpulan Comment Pengguna</h3>
         {data.map((item) => (
           <div className="col-sm-6 mt-2 mb-2">
-            <div key={item.id} className="card">
+            <div className="card">
               <div className="card-body">
-                <h5 className="card-title">{item.name}</h5>
+                <h5 className="card-title" key={item.id}>{item.name}</h5>
                 <p className="card-text">{item.body}</p>
                   <a href="#" className="btn btn-success">
                     Edit Comment
